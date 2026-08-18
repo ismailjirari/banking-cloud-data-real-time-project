c544a9273e16dfd4033038cfd9ba26bd945f0cf8
ismailjirari <ismailjirari5@gmail.com>
2026-08-18 14:20:08 +0100

    Update kafka_to_minio.py

--- consumer/kafka_to_minio.py
+++ consumer/kafka_to_minio.py
@@ -1,6 +1,7 @@
 import json
 import os
 from datetime import datetime, timezone
+
 import boto3
 import pandas as pd
 from dotenv import load_dotenv
@@ -13,29 +14,30 @@ load_dotenv()
 
 # Kafka consumer settings
 consumer = KafkaConsumer(
-    'banking_server.public.customers',
-    'banking_server.public.accounts',
-    'banking_server.public.transactions',
+    "banking_server.public.customers",
+    "banking_server.public.accounts",
+    "banking_server.public.transactions",
     bootstrap_servers=os.getenv("KAFKA_BOOTSTRAP"),
-    auto_offset_reset='earliest',
+    auto_offset_reset="earliest",
     enable_auto_commit=True,
     group_id=os.getenv("KAFKA_GROUP"),
-    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+    value_deserializer=lambda x: json.loads(x.decode("utf-8")),
 )
 
 # MinIO client
 s3 = boto3.client(
-    's3',
+    "s3",
     endpoint_url=os.getenv("MINIO_ENDPOINT"),
     aws_access_key_id=os.getenv("MINIO_ACCESS_KEY"),
-    aws_secret_access_key=os.getenv("MINIO_SECRET_KEY")
+    aws_secret_access_key=os.getenv("MINIO_SECRET_KEY"),
 )
 
 bucket = os.getenv("MINIO_BUCKET")
 
 # Create bucket if not exists
-if bucket not in [b['Name'] for b in s3.list_buckets()['Buckets']]:
+if bucket not in [b["Name"] for b in s3.list_buckets()["Buckets"]]:
     s3.create_bucket(Bucket=bucket)
+
 
 # Consume and write function
 def write_to_minio(table_name, records):
@@ -59,12 +61,13 @@ def write_to_minio(table_name, records):
 
     print(f"✅ Uploaded {len(records)} records to s3://{bucket}/{s3_key}")
 
+
 # Batch consume
 batch_size = 50
 buffer = {
-    'banking_server.public.customers': [],
-    'banking_server.public.accounts': [],
-    'banking_server.public.transactions': []
+    "banking_server.public.customers": [],
+    "banking_server.public.accounts": [],
+    "banking_server.public.transactions": [],
 }
 
 print("✅ Connected to Kafka. Listening for messages...")
@@ -80,5 +83,5 @@ for message in consumer:
         print(f"[{topic}] -> {record}")  # Debugging
 
     if len(buffer[topic]) >= batch_size:
-        write_to_minio(topic.split('.')[-1], buffer[topic])
+        write_to_minio(topic.split(".")[-1], buffer[topic])
         buffer[topic] = []
