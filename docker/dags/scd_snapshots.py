7527150e591b5f23524be0e20dda1f98f9c060de
ismailjirari <ismailjirari5@gmail.com>
2026-08-18 14:07:39 +0100

    Update scd_snapshots.py

--- docker/dags/scd_snapshots.py
+++ docker/dags/scd_snapshots.py
@@ -1,6 +1,9 @@
+from datetime import timedelta
+
+import pendulum
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from datetime import datetime, timedelta
+
 
 default_args = {
     "owner": "airflow",
@@ -9,24 +12,31 @@ default_args = {
     "retry_delay": timedelta(minutes=1),
 }
 
+
 with DAG(
     dag_id="SCD2_snapshots",
     default_args=default_args,
     description="Run dbt snapshots for SCD2",
-    schedule_interval="@daily",     # or "@hourly" depending on your needs
-    start_date=datetime(2025, 9, 1),
+    schedule_interval="@daily",
+    start_date=pendulum.datetime(2025, 9, 1, tz="UTC"),
     catchup=False,
     tags=["dbt", "snapshots"],
 ) as dag:
 
     dbt_snapshot = BashOperator(
         task_id="dbt_snapshot",
-        bash_command="cd /opt/airflow/banking_dbt && dbt snapshot --profiles-dir /home/airflow/.dbt"
-        )
-    dbt_run_marts = BashOperator(
-        task_id="dbt_run_marts",
-        bash_command="cd /opt/airflow/banking_dbt && dbt run --select marts --profiles-dir /home/airflow/.dbt"
+        bash_command=(
+            "cd /opt/airflow/banking_dbt && "
+            "dbt snapshot --profiles-dir /home/airflow/.dbt"
+        ),
     )
 
+    dbt_run_marts = BashOperator(
+        task_id="dbt_run_marts",
+        bash_command=(
+            "cd /opt/airflow/banking_dbt && "
+            "dbt run --select marts --profiles-dir /home/airflow/.dbt"
+        ),
+    )
 
-    dbt_snapshot
+    dbt_snapshot >> dbt_run_marts
